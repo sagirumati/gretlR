658d9f4129d4215c74ff4fa2defa2337f8655be4
sagirumati <smatiweb@gmail.com>
2022-04-25 22:50:58 +0100

    unlink_gretl.R created

--- R/utils.R
+++ R/utils.R
@@ -14,9 +14,12 @@ create_dir=function(x) if(!dir.exists(x)) dir.create(x,recursive = T)
 # system_exec
 
 system_exec=function(){
-  gretlExecFile=eval(expression(gretlExecFile),envir = parent.frame()) # Dynamic scoping
+
+    gretlExecFile=eval(expression(gretlExecFile),envir = parent.frame()) # Dynamic scoping
   system2("gretlcli",paste("-b -e -t",gretlExecFile))
 on.exit(unlink(gretlExecFile),add = T)
+on.exit(unlink_gretl(),add = T)
+
   }
 
 
@@ -36,6 +39,28 @@ kable_format <- function(){
   return(format)
 }
 
+
+
+# unlink_gretl
+
+# unlink_gretl=function(){
+# homePath=path.expand("~")
+# gretl_files=dir(homePath,"\\.gretl",all.files = T) %>%
+# unlink(paste0(homePath,"/",.),recursive = T)
+# }
+
+
+unlink_gretl=function(){
+  homePath=path.expand("~")
+  # gretl_hidden_files=dir(homePath,"^\\.gretl",all.files = T)
+  #   unlink(paste0(homePath,"/",gretl_hidden_files),recursive = T)
+
+    gretl_folder=dir(homePath,"^gretl$",all.files = T)
+if(file.exists(paste0(homePath,'/',gretl_folder))){
+  if(length(dir(paste0(homePath,'/gretl')))==0) unlink(paste0(homePath,"/gretl"),recursive=T)
+}
+
+    }
 
 # writeLines(c("set use_cwd on",code), gretlFile)
 # which(!nzchar(a))
